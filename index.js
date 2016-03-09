1a3a2fb917b6309f58f51b7be4a76f75e0850c74
Nuttapon Pattanavijit <nut@jitta.com>
2016-03-09 13:47:38 +0700

    Able to custom version via param

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var semver = require('semver');
 var packageJson = require('../../package.json');
 
-if (packageJson.engines != null) {
-  var version = process.version
-  var expectedVersion = packageJson.engines.node
+checkVersion = function(customExpectedVersion) {
+  var version = process.version;
+  var expectedVersion = version; // Default: always satisfied
+  if (typeof customExpectedVersion !== "undefined" && customExpectedVersion !== null) {
+    expectedVersion = customExpectedVersion;
+  } else {
+    var packageJson;
+    try {
+      packageJson = require('../../package.json');
+    } catch (e) { return; }
 
-  if (!semver.satisfies(version, expectedVersion)) {
-    throw new Error("Node " + expectedVersion + " required.")
+    engines = packageJson.engines;
+    if (typeof engines !== "undefined" && engines !== null) {
+      expectedVersion = engines.node;
+    }
   }
-}
+
+  if (!semver.satisfies(version, expectedVersion)){
+    throw new Error('Node ' + expectedVersion + ' required.');
+  }
+};
+
+module.exports = checkVersion
